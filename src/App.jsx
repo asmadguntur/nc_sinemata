7ff1f58eaf5e5b80e1cd9514db4b1d69ab960153
asmadguntur <asmadgutnur@gmail.com>
2026-05-16 11:54:32 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  const judul = "Sinemata";
+  return (
+    <div className="p-8">
+      <h1 className="text-3xl font-bold underline">{judul}</h1>
+    </div>
+  );
+}
+
+export default App;
